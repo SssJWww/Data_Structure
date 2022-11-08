b00e32d522e494b8b35735c3cf67ff97d99d9fda
shiwei <401181980@qq.com>
2022-11-08 11:21:25 +0800

    use array implement circle queue

--- src/main/java/com/stone/queue/ArrayCircleQueue.java
+++ src/main/java/com/stone/queue/ArrayCircleQueue.java
@@ -8,7 +8,7 @@ public class ArrayCircleQueue {
     private static int INITIAL_SIZE = 10;
 
     //创建队列初始化的构造器
-    public ArrayCircleQueue(int arrayMaxSize){
+    public ArrayCircleQueue(int arrayMaxSize) {
         this.maxSize = arrayMaxSize;  //如果maxSize为4，实际上数组只能存储3个数据 因为最后一个位置留下判定约束
         this.array = new int[this.maxSize];
         this.front = 0;
@@ -21,7 +21,7 @@ public class ArrayCircleQueue {
 
     //判断队列是否满
     public boolean isFull() {
-        return (rear+1) % maxSize == front;
+        return (rear + 1) % maxSize == front;
     }
 
     //判断队列是否为空
@@ -31,47 +31,47 @@ public class ArrayCircleQueue {
 
     //添加数据到队列中
     public void add(int n) {
-        if(isFull()) {
+        if (isFull()) {
             System.out.println("Queue is full");
             return;
         }
 
         array[rear] = n;
-        rear = (rear+1)%maxSize;
+        rear = (rear + 1) % maxSize;
     }
 
     //获取队列数据
-    public int getQueue(){
+    public int getQueue() {
         //判断队列是否为空
-        if(isEmpty()){
+        if (isEmpty()) {
             throw new RuntimeException("队列为空");
         }
         int value = array[front];
-        front = (front+1)%maxSize;
+        front = (front + 1) % maxSize;
         return value;
     }
 
     //获取队列所有数据
     public void showQueue() {
-        if(isEmpty()) {
+        if (isEmpty()) {
             throw new RuntimeException("队列为空");
         }
 
-        for(int i=front; i<front+size(); i++){
-            System.out.printf("arr[%d]=%d\n",i%maxSize,array[i%maxSize]);
+        for (int i = front; i < front + size(); i++) {
+            System.out.printf("arr[%d]=%d\n", i % maxSize, array[i % maxSize]);
         }
     }
 
     //显示队列头部数据
-    public void showHead(){
-        if(isEmpty()) {
+    public void showHead() {
+        if (isEmpty()) {
             throw new RuntimeException("队列为空");
         }
-        System.out.println("头部数据为："+array[front]);
+        System.out.println("头部数据为：" + array[front]);
     }
 
     //求出当前队列有效数据个数
-    public int size(){
-        return (rear+maxSize-front) % maxSize;
+    public int size() {
+        return (rear + maxSize - front) % maxSize;
     }
 }
