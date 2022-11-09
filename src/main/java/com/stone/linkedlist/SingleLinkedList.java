c11c8243739ba50746dc0e92cc12950bfce49980
shiwei <401181980@qq.com>
2022-11-09 18:47:46 +0800

    single linked list reverse method 1 && 2

--- src/main/java/com/stone/linkedlist/SingleLinkedList.java
+++ src/main/java/com/stone/linkedlist/SingleLinkedList.java
@@ -1,5 +1,7 @@
 package com.stone.linkedlist;
 
+import java.util.ArrayList;
+
 public class SingleLinkedList {
     //初始化头节点，头节点不要动 不存储数据
     private Node head = new Node(0,"head","head");
@@ -53,14 +55,48 @@ public class SingleLinkedList {
 
         Node beforeTemp = head;
         Node temp = head.next;
+        boolean isExist = false;
 
         while (true){
-            if(temp.equals(node)) {
-                beforeTemp.next = node.next;
+            if(temp == null) {break;} //遍历结束
+            if(temp.id == node.id) {
+                isExist = true;
                 break;
             }
             beforeTemp = temp;
             temp = temp.next;
+        }
+
+        if(isExist) {
+            beforeTemp.next = temp.next;
+        }
+    }
+
+    //修改节点
+    public void updateNode(Node node) {
+        if (isEmpty()){
+            System.out.println("链表为空");
+            return;
+        }
+        Node temp = head.next;
+        int nodeId = node.id;
+        boolean isExist = false;
+
+        while (true){
+            if (temp == null) {break;} //遍历结束
+            if(temp.id == nodeId) {
+                isExist = true;
+                break;
+            }
+            temp = temp.next;
+        }
+
+        if(isExist) {
+            temp.name = node.name;
+            temp.nickName = node.nickName;
+            System.out.println("修改完成");
+        } else {
+            System.out.println("要修改的节点不存在");
         }
     }
 
@@ -84,5 +120,80 @@ public class SingleLinkedList {
     //判断链表是否为空
     public boolean isEmpty(){
         return head.next == null;
+    }
+
+    /**
+     * 单链表面试题
+     */
+    //1.求但链表中节点个数
+    public int count(){
+        if (isEmpty()) {
+            return 0;
+        }
+        Node temp = head;
+        int count = 0;
+        while (true) {
+            if(temp.next == null){
+                break;
+            }
+            temp = temp.next;
+            count++;
+        }
+        return count;
+    }
+
+    //查找倒数第k个节点
+    public void searchByReverseOrder(int order){
+        if(order>count() || order < 1){
+            System.out.println("链表长度小于查找节点数，故没有");
+        }
+        if (isEmpty()) {
+            System.out.println("链表为空");
+        }
+        Node temp = head;
+        for (int i=0;i<count()-order+1;i++) {
+            temp = temp.next;
+        }
+
+        System.out.println("查找节点为"+temp);
+    }
+
+    //单链表的反转
+    public void reverse(){
+        if (isEmpty()) {
+            System.out.println("链表为空");
+        }
+
+        ArrayList<Node> list = new ArrayList<>();
+        Node temp = head.next;
+        while (true) {
+            if (temp == null){break;}
+            list.add(temp);
+            temp = temp.next;
+        }
+        temp = head;
+        for (int i=list.size()-1;i>=0;i--){
+            temp.next = list.get(i);
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    public void reverse2(){
+        if (isEmpty() || count()==1) {
+            System.out.println("无法处理");
+        }
+
+        Node reverseHead = new Node();
+        Node cur = head.next;
+        Node next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = reverseHead.next;
+            reverseHead.next = cur;
+            cur = next;
+        }
+        head.next = reverseHead.next;
+
     }
 }
--- src/main/java/com/stone/linkedlist/SingleLinkedListDemo.java
+++ src/main/java/com/stone/linkedlist/SingleLinkedListDemo.java
@@ -7,6 +7,8 @@ public class SingleLinkedListDemo {
         Node node3 = new Node(3,"3","3");
         Node node4 = new Node(4,"4","4");
         Node node5 = new Node(5,"5","5");
+        Node node6 = new Node(6,"6","6");
+
 
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         singleLinkedList.addByOrder(node1);
@@ -14,7 +16,16 @@ public class SingleLinkedListDemo {
         singleLinkedList.addByOrder(node5);
         singleLinkedList.addByOrder(node4);
         singleLinkedList.addByOrder(node3);
-//        singleLinkedList.deleteNode(node3);
+        node5.name = "五";
+
+//        singleLinkedList.updateNode(node6);
+//        singleLinkedList.deleteNode(node1);
+        singleLinkedList.showList();
+//        int count = singleLinkedList.count();
+//        System.out.println(count);
+//        singleLinkedList.searchByReverseOrder(3);
+        singleLinkedList.reverse2();
+        System.out.println("反转后的链表～～～～～～～～～～～～～～～～～");
         singleLinkedList.showList();
     }
 }
